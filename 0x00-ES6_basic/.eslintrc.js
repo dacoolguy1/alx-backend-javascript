b8c4141bc1c33cffc51beac5286b0887bb5307e2
Emmanuel Temiede <68757523+dacoolguy1@users.noreply.github.com>
2023-06-26 23:42:58 +0100

    Update .eslintrc.js

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -1,26 +1,28 @@
 module.exports = {
-  'env': {
-    'browser': true,
-    'commonjs': true,
-    'es2021': true,
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
   },
-  'extends': 'google',
-  'overrides': [
+  extends: ['airbnb-base', 'plugin:jest/all'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': ['error', 'LabeledStatement', 'WithStatement'],
+  },
+  overrides: [
     {
-      'env': {
-        'node': true,
-      },
-      'files': [
-        '.eslintrc.{js,cjs}',
-      ],
-      'parserOptions': {
-        'sourceType': 'script',
-      },
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
     },
   ],
-  'parserOptions': {
-    'ecmaVersion': 'latest',
-  },
-  'rules': {
-  },
 };
