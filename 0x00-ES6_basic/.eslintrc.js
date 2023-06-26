17dfb883c773fbe055b7033b9a189bc221a13c45
Emmanuel Temiede <68757523+dacoolguy1@users.noreply.github.com>
2023-06-26 23:45:05 +0100

    Update .eslintrc.js

--- 0x00-ES6_basic/.eslintrc.js
+++ 0x00-ES6_basic/.eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     es6: true,
     jest: true,
   },
-  extends: ['airbnb-base', 'plugin:jest/all'],
+  extends: ['google', 'plugin:jest/all'],
   globals: {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
