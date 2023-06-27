1a408c1465b4b2b33a1a3d5f40c72d8b3fa159bf
Emmanuel Temiede <68757523+dacoolguy1@users.noreply.github.com>
2023-06-27 13:20:26 +0100

    Update 0-constants.js

--- 0x00-ES6_basic/0-constants.js
+++ 0x00-ES6_basic/0-constants.js
@@ -1,15 +1,24 @@
+/**
+ * Returns the first task.
+ * @return {string} The first task.
+ */
 export function taskFirst() {
   const task = 'I prefer const when I can.';
   return task;
 }
-
+/**
+ * Returns the last part of a task.
+ * @return {string} The last part of a task.
+ */
 export function getLast() {
   return ' is okay';
 }
-
+/**
+ * Returns a combination of task and last part.
+ * @return {string} The combination of task and last part.
+ */
 export function taskNext() {
   let combination = 'But sometimes let';
   combination += getLast();
-
   return combination;
 }
